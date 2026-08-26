402eb5a7a00cd0ed6780dd6e42a7f68a44016757
Taha Iqbal <low195@usask.ca>
2026-08-26 14:04:20 -0600

    Updated testbench to support other 6 operations

--- testbench/tb_alu.sv
+++ testbench/tb_alu.sv
@@ -1,7 +1,7 @@
 module tb_alu;
     logic [7:0] A;
     logic [7:0] B;
-    logic operation;
+    logic [2:0] operation;
     logic [7:0] result; 
 
     alu dut(
@@ -14,17 +14,59 @@ module tb_alu;
         $dumpfile("alu_wave.vcd");
         $dumpvars(0, tb_alu);
         
-        // Test Case 1: A = 3, B = 5, operation = 0 (Addition)
+        // Test Case 1: A = 3, B = 5, operation = 000 (Addition)
         A = 8'd3; 
         B = 8'd5; 
-        operation = 1'b0; 
+        operation = 3'b000; 
         #10; 
         $display("A=%d B=%d operation=%b result=%d", A, B, operation, result);
 
-        //Test Case 2: A = 3, B = 5, operation = 1 (AND)
+         //Test Case 2: A = 3, B = 5, operation = 001 (Subtraction)
         A = 8'd3;
         B = 8'd5; 
-        operation = 1'b1;
+        operation = 3'b001;
+        #10;
+        $display("A=%d B=%d operation=%b result=%d", A, B, operation, result);
+
+        //Test Case 3: A = 3, B = 5, operation = 010 (AND)
+        A = 8'd3;
+        B = 8'd5; 
+        operation = 3'b010;
+        #10;
+        $display("A=%d B=%d operation=%b result=%d", A, B, operation, result);
+
+         //Test Case 4: A = 3, B = 5, operation = 011 (OR)
+        A = 8'd3;
+        B = 8'd5; 
+        operation = 3'b011;
+        #10;
+        $display("A=%d B=%d operation=%b result=%d", A, B, operation, result);
+
+         //Test Case 5: A = 3, B = 5, operation = 100 (XOR)
+        A = 8'd3;
+        B = 8'd5; 
+        operation = 3'b100;
+        #10;
+        $display("A=%d B=%d operation=%b result=%d", A, B, operation, result);
+
+         //Test Case 6: A = 3, B = 5, operation = 101 (SLL)
+        A = 8'd3;
+        B = 8'd5; 
+        operation = 3'b101;
+        #10;
+        $display("A=%d B=%d operation=%b result=%d", A, B, operation, result);
+
+         //Test Case 7: A = 3, B = 5, operation = 110 (SRL)
+        A = 8'd3;
+        B = 8'd5; 
+        operation = 3'b110;
+        #10;
+        $display("A=%d B=%d operation=%b result=%d", A, B, operation, result);
+
+         //Test Case 8: A = 5, B = 5, operation = 111 (Comparison)
+        A = 8'd5;
+        B = 8'd5; 
+        operation = 3'b111;
         #10;
         $display("A=%d B=%d operation=%b result=%d", A, B, operation, result);
     end
