6a09ca0de1e0f6cd79a750d5c48d9d195f56bf70
Taha Iqbal <low195@usask.ca>
2026-08-27 17:00:33 -0600

    Added reset feature to d flip flop and its testbench

--- rtl/d_flip_flop.sv
+++ rtl/d_flip_flop.sv
@@ -1,10 +1,14 @@
 module d_flip_flop(
     input logic clk,
     input logic D,
-    output logic Q
+    output logic Q,
+    input logic reset
 ); 
-    always_ff @(posedge clk) begin
-        Q <= D; 
+    always_ff @(posedge clk or posedge reset) begin 
+        if (reset) //Reset button 
+            Q <= 1'b0; 
+        else
+            Q <= D; 
     end 
 endmodule 
    
--- testbench/tb_d_flip_flop.sv
+++ testbench/tb_d_flip_flop.sv
@@ -1,12 +1,14 @@
 module tb_d_flip_flop; 
     logic clk = 0;
     logic D;
-    logic Q; 
+    logic Q;
+    logic reset = 0;  
 
 d_flip_flop dut (
     .D(D),
     .Q(Q), 
-    .clk(clk) 
+    .clk(clk),
+    .reset(reset)
 ); 
 
     always #5 clk = ~clk; 
@@ -28,5 +30,26 @@ d_flip_flop dut (
             $display("Test Case 2 Passed: Q is %0d", Q); 
         else 
             $error("Test Case 2 Failed: Expected 1 got %0d", Q); 
+        
+        //Test Case 3: D = 1, Synchronous Reset 
+        @(posedge clk);
+        reset = 1;  
+        #1; 
+        if (Q == 0)
+            $display("Test Case 3 Passed: Q is %0d", Q); 
+        else 
+            $error("Test Case 3 Failed: Expected 0 got %0d", Q); 
+        
+        //Test Case 4: D = 1, Asynchronous Reset 
+        reset = 0;
+        D = 1;
+        @(posedge clk); //Setting Q to 1 
+        #1;  
+        reset = 1; 
+        #1; 
+        if (Q == 0)
+            $display("Test Case 4 Passed: Q is %0d", Q); 
+        else 
+            $error("Test Case 4 Failed: Expected 0 got %0d", Q); 
     end
 endmodule 
