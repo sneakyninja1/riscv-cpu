6c8804a1722f2cbd14e07e9d9a1a12e7fd3aafb0
Taha Iqbal <low195@usask.ca>
2026-08-31 13:26:07 -0600

    Foundation laid for register file, need to add the x0 rule

---
+++ rtl/register_file.sv
@@ -0,0 +1,24 @@
+module register_file(
+    input logic clk, 
+    input logic [4:0] read_addr_1, 
+    input logic [4:0] read_addr_2,
+    input logic [4:0] write_addr, 
+    input logic [31:0] write_data,
+    input logic write_enable, 
+    output logic [31:0] data_1, 
+    output logic [31:0] data_2,
+    input logic reset
+); 
+    logic [31:0] registers [0:31]; //32 bit wide, 32 registers
+    assign data_1 = registers[read_addr_1]; 
+    assign data_2 = registers[read_addr_2];  
+
+    always_ff @(posedge clk or posedge reset) begin 
+        if (reset) //Reset all registers to 0 
+            for (int i = 0; i < 32; i++) begin
+                registers[i] <= 0; 
+            end
+        else if (write_enable == 1) // if control signal positive, store the data in the registers 
+            registers[write_addr] <= write_data; 
+    end 
+endmodule 
