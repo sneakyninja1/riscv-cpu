4f87454a3fc73f13212fd490d3b43729fa735d7e
Taha Iqbal <low195@usask.ca>
2026-09-01 13:57:18 -0600

    Finished testbench. Added 2 more tests to check writing while the control signal is off and checking 2 registers simultaneously

--- testbench/tb_register_file.sv
+++ testbench/tb_register_file.sv
@@ -74,8 +74,24 @@ register_file dut(
         #1; 
         read_addr_1 = write_addr; 
         #1; 
-        if (data_1 == )
-
-
-
+        if (data_1 == 0)
+            $display("Test Case 4 Passed.");  
+        else
+            $error("Test Case 4 Failed. Expected 0, got %0d", data_1);
+    
+        //Test Case 5 Checking 2 data ports simultaneously 
+        write_addr = 5'd10; 
+        write_data = 32'd77; 
+        write_enable = 1; 
+        #1; 
+        @(posedge clk); 
+        #1; 
+        read_addr_2 = write_addr; 
+        read_addr_1 = 5'd2; 
+        #1; 
+        if (data_1 == 48 && data_2 == 77)
+            $display("Test Case 5 Passed."); 
+        else
+            $error("Test Case 5 Failed. Expected data1 to be 48 but got %0d, and data2 to be 77 but got %0d", data_1, data_2); 
+    end 
 endmodule 
